3f466330837947ad0206cd238698a84ea09c3469
Farhan Butt <farhan.butt5555@gmail.com>
2019-09-07 23:23:03 +0500

    Add project files.

---
+++ Controllers/firstController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assessment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assessment.Controllers
+{
+    public class firstController : Controller
+    {
+        private ASMTContext ORM = null;
+        private ASMTContext _ORM;
+
+        public firstController(ASMTContext _ORM)
+        {
+            ORM = _ORM;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category c)
+          {
+            ORM.Category.Add(c);
+
+            ORM.SaveChanges();
+            return View();
+    }
+
+        [HttpGet]
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost(Post p)
+        {
+            ORM.Post.Add(p);
+
+            ORM.SaveChanges();
+            return View();
+        }
+
+
+
+    }
+}
+
